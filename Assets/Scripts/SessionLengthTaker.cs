8d874b0e2e45ff049d85b0d638eca54b1c6a0fe5
caelum.sloan1 <caelum.sloan1@ucalgary.ca>
2018-12-28 10:10:37 -0700

    First commit

---
+++ Assets/Scripts/SessionLengthTaker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SessionLengthTaker : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private UnityEngine.UI.Text sliderText;
+    [HideInInspector] public float sliderValue=60;
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            slider.gameObject.SetActive(false);
+        }
+        sliderText.text = sliderValue.ToString() + " minutes";
+	}
+
+    public void UpdateSliderValue()
+    {
+        sliderValue = slider.value;
+    }
+}
