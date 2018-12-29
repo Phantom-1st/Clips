c1c315e27c6b06efb880b96ff64fbd7372415742
Phantom-1st <caelum.is.amazing@gmail.com>
2018-12-28 21:22:14 -0700

    Fuck stupid gitignore

--- Assets/Scriptable Objects/Definitions/TextQueue.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Text/TextQueue")]
-public class TextQueue : ScriptableObject
-{
-    public List<Text> texts = new List<Text>();
-}
